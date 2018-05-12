76a7f3577a242b182f6f26b2ed93caaf82cbb3ea
Rob Blake <robbie.a.blake@gmail.com>
2018-05-12 13:10:01 +0200

    Application first committed.

---
+++ Code/WordLibrary/Gui/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Control draggedItem;
+        Point itemRelativePosition;
+        bool IsDragging;
+        List<WordButton> buttons = new List<WordButton>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IsDragging = false;
+        }
+
+        private void LoadWords(string[] words)
+        {
+            Random r = new Random();
+
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrWhiteSpace(word))
+                {
+                    WordButton button = new WordButton { Content = word };
+                    button.PreviewMouseLeftButtonDown += btn_PreviewMouseLeftButtonDown;
+                    button.PreviewMouseLeftButtonUp += btn_PreviewMouseLeftButtonUp;
+                    button.PreviewMouseMove += btn_PreviewMouseMove;
+                    //button.Click += Button_Click;
+
+                    buttons.Add(button);
+                    WordCanvas.Children.Add(button);
+
+                    Canvas.SetLeft(button, r.Next(0, (int)(this.Width - button.Width - 50)));
+                    Canvas.SetTop(button, r.Next(0, (int)((this.Height - 250) - button.Height)));
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            IsDragging = true;
+            draggedItem = (Button)sender;
+            itemRelativePosition = e.GetPosition(draggedItem);
+        }
+
+        private void btn_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsDragging)
+                return;
+
+            IsDragging = false;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] words = File.ReadAllLines(openFileDialog.FileName);
+                MessageBoxResult result = MessageBox.Show(this, "Would you like to remove the currently loaded set?", "Load Words", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                    WordCanvas.Children.Clear();
+                LoadWords(words);
+            }
+        }
+
+        private void btn_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!IsDragging)
+                return;
+
+            Point canvasRelativePosition = e.GetPosition(WordCanvas);
+
+            Canvas.SetTop(draggedItem, canvasRelativePosition.Y - itemRelativePosition.Y);
+            Canvas.SetLeft(draggedItem, canvasRelativePosition.X - itemRelativePosition.X);
+        }
+    }
+}
---
+++ Code/WordLibrary/Gui/WordButton.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Gui
+{
+    class WordButton : Button
+    {
+        public WordButton()
+        {
+            Id = Guid.NewGuid().ToString();
+            FontSize = 24;
+            Canvas.SetZIndex(this, 0);
+            ToolTip = Canvas.GetZIndex(this).ToString();
+            this.GotFocus += (s, e) =>
+            {
+                FontWeight = FontWeights.Bold;
+                this.Foreground = new SolidColorBrush(Colors.Blue);
+                BringToFront((Canvas)this.Parent, this);
+            };
+            this.LostFocus += (s, e) =>
+            {
+                FontWeight = FontWeights.Normal;
+                this.Foreground = new SolidColorBrush(Colors.Black);
+                Canvas.SetZIndex(this, 99);
+                ToolTip = Canvas.GetZIndex(this).ToString();
+            };
+        }
+        public string Id { get; private set; }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+            Size size = MeasureTextSize(this.Content.ToString(), this.FontFamily, this.FontStyle, this.FontWeight, this.FontStretch, this.FontSize);
+            this.Width = size.Width + 50;
+            this.Height = size.Height + 50;
+        }
+
+        
+
+        /// <summary>
+        /// Get the required height and width of the specified text. Uses FortammedText
+        /// </summary>
+        public static Size MeasureTextSize(string text, FontFamily fontFamily, FontStyle fontStyle, FontWeight fontWeight, FontStretch fontStretch, double fontSize)
+        {
+            FormattedText ft = new FormattedText(text,
+                                                 CultureInfo.CurrentCulture,
+                                                 FlowDirection.LeftToRight,
+                                                 new Typeface(fontFamily, fontStyle, fontWeight, fontStretch),
+                                                 fontSize,
+                                                 Brushes.Black);
+            return new Size(ft.Width, ft.Height);
+        }
+
+        /// <summary>
+        /// Get the required height and width of the specified text. Uses Glyph's
+        /// </summary>
+        public static Size MeasureText(string text, FontFamily fontFamily, FontStyle fontStyle, FontWeight fontWeight, FontStretch fontStretch, double fontSize)
+        {
+            Typeface typeface = new Typeface(fontFamily, fontStyle, fontWeight, fontStretch);
+            GlyphTypeface glyphTypeface;
+
+            if (!typeface.TryGetGlyphTypeface(out glyphTypeface))
+            {
+                return MeasureTextSize(text, fontFamily, fontStyle, fontWeight, fontStretch, fontSize);
+            }
+
+            double totalWidth = 0;
+            double height = 0;
+
+            for (int n = 0; n < text.Length; n++)
+            {
+                ushort glyphIndex = glyphTypeface.CharacterToGlyphMap[text[n]];
+
+                double width = glyphTypeface.AdvanceWidths[glyphIndex] * fontSize;
+
+                double glyphHeight = glyphTypeface.AdvanceHeights[glyphIndex] * fontSize;
+
+                if (glyphHeight > height)
+                {
+                    height = glyphHeight;
+                }
+                totalWidth += width;
+            }
+
+            return new Size(totalWidth, height);
+        }
+
+        static public void BringToFront(Canvas pParent, WordButton pToMove)
+        {
+            try
+            {
+                int currentIndex = Canvas.GetZIndex(pToMove);
+                int zIndex = 0;
+                int maxZ = 0;
+                WordButton child;
+                for (int i = 0; i < pParent.Children.Count; i++)
+                {
+                    if (pParent.Children[i] is WordButton &&
+                        pParent.Children[i] != pToMove)
+                    {
+                        child = pParent.Children[i] as WordButton;
+                        zIndex = Canvas.GetZIndex(child);
+                        maxZ = Math.Max(maxZ, zIndex);
+                        if (zIndex > currentIndex)
+                        {
+                            Canvas.SetZIndex(child, zIndex - 1);
+                        }
+                    }
+                }
+                Canvas.SetZIndex(pToMove, maxZ);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+    }
+        }
